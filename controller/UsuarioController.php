7256695bd8fff60b6fa50baeb7219230eb0ecb8f
Guilherme Brito de Almeida Costa <guilhermebritoalmeidacosta@gmail.com>
2021-06-08 17:06:07 -0300

    Função delete do controller

--- controller/UsuarioController.php
+++ controller/UsuarioController.php
@@ -36,18 +36,16 @@ class UsuarioController{
             {
                 session_start();
 
-                $_SESSION["email"] = $_POST["email"];
-                $_SESSION["senha"] = $_POST["senha"];
-
-                $obj->setSenha($_POST["senha"]);
-                $obj->setEmail($_POST["email"]);
-
                 $id = $obj->getIdBD();
                 $nome = $obj->getNomeBD();
-                
+
+                $_SESSION["email"] = $_POST["email"];
+                $_SESSION["senha"] = $_POST["senha"];
                 $_SESSION["id"] = $id;
                 $_SESSION["nome"] = $nome;
 
+                $obj->setSenha($_POST["senha"]);
+                $obj->setEmail($_POST["email"]);
                 $obj->setId($id);
                 $obj->setNome($nome);
    
@@ -144,14 +142,55 @@ class UsuarioController{
 
     public function delete(){
 
-        if( !isset($_GET['id']) ){
+        if( !isset($_SESSION["id"]) )
+        {
             echo "Id não informado";
             exit;
         }
 
+        if( !isset($_POST["senha"]) )
+        {
+            echo "Senha não informado";
+            exit;
+        }
+
+        try
+        {
         $obj = new Usuario();
-        $obj->setId($_GET['id']);
-        $obj->delete();       
+
+        $con = $obj->getCon();
+
+        $con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+        $query = "SELECT * FROM usuario WHERE email = :email AND senha = :senha";
+            
+        $user = $con->prepare($query);
+
+            $user->execute(
+                    array(
+                        'email' => $_SESSION["email"],
+                        'senha' => $_POST["senha"]
+                    )
+                );
+
+        $count = $user->rowCount();
+
+        if($count > 0)
+        {
+            $obj = new Usuario();
+            $obj->delete();  
+        }
+        else
+        {
+            echo "Senha incorreta";
+        }
+
+        }
+        catch(PDOException $error)
+        {
+            echo $error->getMessage();
+        }
+    
     }
 
 }
