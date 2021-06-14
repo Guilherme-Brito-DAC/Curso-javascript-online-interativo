9e4ef32e29fb6eabc58b632c88747123af129dbb
Guilherme Brito de Almeida Costa <guilhermebritoalmeidacosta@gmail.com>
2021-06-13 22:46:27 -0300

    Update banco de dados

--- banco_de_dados/curso.sql
+++ banco_de_dados/curso.sql
@@ -1,57 +1,128 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE SCHEMA IF NOT EXISTS `curso` DEFAULT CHARACTER SET utf8 ;
-USE `curso` ;
 
-CREATE TABLE IF NOT EXISTS `curso`.`usuario` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `usuario` VARCHAR(20) NOT NULL,
-  `nome` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `senha` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC))
-ENGINE = InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE IF NOT EXISTS `curso`.`atividades` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `codigo` TEXT NOT NULL,
-  `data_postagem` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `questao_id` INT NOT NULL,
-  `usuario_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_artigo_usuario_idx` (`usuario_id` ASC),
-  CONSTRAINT `fk_artigo_usuario`
-    FOREIGN KEY (`usuario_id`)
-    REFERENCES `curso`.`usuario` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Banco de dados: `curso`
+--
 
-CREATE TABLE IF NOT EXISTS `curso`.`comentario` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `mensagem` VARCHAR(300) NOT NULL,
-  `data_postagem` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `questao_id` INT NOT NULL,
-  `usuario_id` INT NOT NULL,
-  `atividades_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_comentario_usuario1_idx` (`usuario_id` ASC),
-  INDEX `fk_comentario_atividades1_idx` (`atividades_id` ASC),
-  CONSTRAINT `fk_comentario_usuario1`
-    FOREIGN KEY (`usuario_id`)
-    REFERENCES `curso`.`usuario` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_comentario_atividades1`
-    FOREIGN KEY (`atividades_id`)
-    REFERENCES `curso`.`atividades` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+--
+-- Estrutura da tabela `atividades`
+--
+
+CREATE TABLE `atividades` (
+  `id` int(11) NOT NULL,
+  `codigo` text NOT NULL,
+  `data_postagem` datetime NOT NULL DEFAULT current_timestamp(),
+  `questao_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `comentario`
+--
+
+CREATE TABLE `comentario` (
+  `id` int(11) NOT NULL,
+  `mensagem` varchar(300) NOT NULL,
+  `data_postagem` datetime NOT NULL DEFAULT current_timestamp(),
+  `questao_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `atividades_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `senha` varchar(45) DEFAULT NULL,
+  `img_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `atividades`
+--
+ALTER TABLE `atividades`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_artigo_usuario_idx` (`usuario_id`);
+
+--
+-- Índices para tabela `comentario`
+--
+ALTER TABLE `comentario`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_comentario_usuario1_idx` (`usuario_id`),
+  ADD KEY `fk_comentario_atividades1_idx` (`atividades_id`);
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `atividades`
+--
+ALTER TABLE `atividades`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `comentario`
+--
+ALTER TABLE `comentario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `atividades`
+--
+ALTER TABLE `atividades`
+  ADD CONSTRAINT `fk_artigo_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `comentario`
+--
+ALTER TABLE `comentario`
+  ADD CONSTRAINT `fk_comentario_atividades1` FOREIGN KEY (`atividades_id`) REFERENCES `atividades` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_comentario_usuario1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
