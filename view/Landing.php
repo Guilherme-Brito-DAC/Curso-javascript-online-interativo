c6aeadf37c6546b691a2033005a75a0de53d77d7
Maria Fernandes <81184763+mariafe03@users.noreply.github.com>
2021-07-29 20:20:19 -0300

    Update Landing.php

--- view/Landing.php
+++ view/Landing.php
@@ -16,9 +16,9 @@
                 echo "<img class='logojs' src='img/logo.png'>"; 
                 ?>
             <div class="text">
-                <h1>Curso JavaScipt</h1>
+                <h1>Curso JavaScript</h1>
                 <br>
-                <h2>Com este treinamento você será capaz de desenvolver seus códigos javascript para sites e sistemas que rodam no browser. O objetivo é fazer você conseguir testar seu código em tempo real. Se quer aprender tudo de javascript, está no lugar certo.</h2>
+                <h2>Com este treinamento, você será capaz de desenvolver programações em JavaSCript para sites e sistemas que rodam no browser. O objetivo é fazer com que você aprenda enquanto testa seus códigos em tempo real. Se quer saber tudo de javascript, está no lugar certo.</h2>
                 <a style=" text-decoration: none; color: white; width: 20%;" class="btn_entrar" href="login.php">Logar</a></button>
             </div>
         </div>
